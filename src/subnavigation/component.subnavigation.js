7c13744c6eb25c192b44c7e46725dd482decf94d
Evan Short <evan.short@gmail.com>
2017-12-11 20:30:42 -0500

    allowing a single element through

--- src/subnavigation/component.subnavigation.js
+++ src/subnavigation/component.subnavigation.js
@@ -1,9 +1,10 @@
 import React from 'react';
 class SubNavigation extends React.Component {
   render() {
+		const children = this.props.children.map?this.props.children:[this.props.children];
     return <ul className="link-list">
         {
-          this.props.children.map(
+          children.map(
           (child, key) =>
             <li key={key} className={(child.props.to&&this.props.navigation.getIn(["data", "location"]).match(child.props.to)? "is-selected" : "")+" link-list-element"}>
               {
